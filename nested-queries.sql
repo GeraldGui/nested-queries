a956bc398f672faa4074a1dfefcaddd57c30fc76
Gerald G <131211681+GeraldGui@users.noreply.github.com>
2026-06-02 14:35:25 -0400

    Add files via upload

--- nested-queries.sql
+++ nested-queries.sql
@@ -14,14 +14,22 @@ WHERE ShipVia IN (SELECT ShipperID
                 WHERE ShipperID = 3);
                 
 -- Question 3
-SELECT OrderID, ProductID
+SELECT OrderID
 FROM `order details`
 WHERE ProductID = (SELECT ProductID
                    FROM products
                    WHERE ProductName LIKE "%Sasquatch Ale%");
                 
 -- Question 4
-
+SELECT FirstName, LastName
+FROM employees
+WHERE EmployeeID = (SELECT EmployeeID
+					FROM orders
+                    WHERE OrderID = 10266);
 
 -- Question 5
-                
+SELECT CompanyName
+FROM customers
+WHERE CustomerID = (SELECT CustomerID
+					FROM orders
+                    WHERE OrderID = 10266);
